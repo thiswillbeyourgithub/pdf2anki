bfe05a53ed4663759174b0eec2d82646e91000a7
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-03-01 22:48:49 +0100

    minor

--- pdf2anki.py
+++ pdf2anki.py
@@ -37,9 +37,11 @@ from tqdm import tqdm
 
 
 ######### SETTINGS :
-# iterate over all pdfs, turn them into picture and extract text
-PDF_dir="/home/USERNAME/Downloads/temp/"
-ankiMediaFolder="/home/USERNAME/.local/share/Anki2/PROFILE_NAME/collection.media/"
+username = "FILLME"
+profile_name = "Main"
+#
+PDF_dir=f"/home/{username}/Downloads/temp/"
+ankiMediaFolder=f"/home/{username}/.local/share/Anki2/{profile_name}/collection.media/"
 ######### 
 
 
