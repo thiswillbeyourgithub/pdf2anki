9915085548c59b7ec0917042cb4a7c27797f1080
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-03-02 15:10:30 +0100

    parallel processing + new settings + fstrings + misc

--- pdf2anki.py
+++ pdf2anki.py
@@ -15,33 +15,35 @@ This file is part of pdf2anki.
 
     You should have received a copy of the GNU General Public License along
     with pdf2anki.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 
 
 
+import time
 import requests
 import subprocess
-import re
+import os
+import PyPDF2
+import pdf2image
 from pathlib import Path
 from tqdm import tqdm
 
-# tested with python3 and anki 2.1.33
-# you need the addon anki-connect
-
-# use pdftk to burst a PDF into single pages like so :
-#   pdftk masterPDF.pdf burst
-# I recommend uding "rename" package to rename the individual pages
-# into something more legible, and this way the sort field contain some information like document name etc :
-#   rename 's/^pg_00/my_document_/' *.pdf
-# don't forget to remove the large pdf from the folder to only import the single page pdf
+from joblib import Parallel, delayed
+import multiprocessing
 
 
 ######### SETTINGS :
+picture_quality=300 #default is 300
+color_nb = "disabled" # default is "disabled", it's the number of colors in the picture
+store_picture = "yes" #default is yes
+
 username = "FILLME"
 profile_name = "Main"
-#
-PDF_dir=f"/home/{username}/Downloads/temp/"
+PDF_dir=f"/home/{username}/Downloads/PDF/"
 ankiMediaFolder=f"/home/{username}/.local/share/Anki2/{profile_name}/collection.media/"
+
+num_cores = max(multiprocessing.cpu_count()-1, 1)
 ######### 
 
 
@@ -69,9 +71,8 @@ def createBasicTemplate():
             ]
         }
     })
-    #print(str(r.json())) # debug in case of error
-    print("Finished template creation!")
-    print("")
+    #print(str(r.json())) # to debug
+    print("Finished template creation!\n")
 
 
 def createImportDeck():
@@ -82,11 +83,10 @@ def createImportDeck():
             "deck": "Imported from PDF",
         }
     })
-    print("Finished deck creation!")
-    print("")
+    print("Finished deck creation!\n")
 
 
-def sendPDFPageToAnki(PNG_name, back):
+def sendToAnki(JPG_name, back):
     r = requests.post('http://127.0.0.1:8765', json={
         "action": "addNote",
         "version": 6,
@@ -95,7 +95,7 @@ def sendPDFPageToAnki(PNG_name, back):
                 "deckName": "Imported from PDF",
                 "modelName": "PDF_per_page",
                 "fields": {
-                    "OnePage": str(''.join(['<img class="" src="', PNG_name,'">'])),
+                    "OnePage": str(''.join(['<img class="" src="', JPG_name,'">'])),
                     "Text": back ,
                 },
                 "tags": [
@@ -106,33 +106,54 @@ def sendPDFPageToAnki(PNG_name, back):
     })
     tqdm.write(str(r.json()))
 
+def batch_convert(PDF_path):
+        " converts single page pdf to jpg and moves them to the media folder "
+        JPG_path = f"{ankiMediaFolder}{PDF_path[len(PDF_dir):]}.jpg"
+        os.system(f'convert \
+                -density {picture_quality} \
+                {color_arg} \
+                -background "#ffffff" \
+                -quiet\
+                {PDF_path} {JPG_path}')
+        return
 
-####### Initialization
 print("Adding Template...")
 createBasicTemplate()
+
 print("Adding destination Deck...")
 createImportDeck()
-####### 
 
-
-####### Main :
 paths = Path(PDF_dir).glob('./*.pdf')
-paths = sorted(list(paths)) # otherwise it's this weird generator type thingie
+paths = sorted(list(paths)) #get a list instead of a generator
+#pdf_file = PdfFileReader(open(args.inputpdf, 'rb'))
+#for page in pdf_file.pages:
+#        PDF_text = page.extractText()
 
+if store_picture == "yes":
+    if color_nb == "disabled":
+        color_arg = ""
+    else:
+        color_arg = f"-colors {color_nb}"
+    print(f"Batch converting images + moving them to the collection using {num_cores} cores...")
+    Parallel(n_jobs=num_cores)(delayed(batch_convert)(str(i)) for i in tqdm(paths))
+
+print("Extracting text...")
 for PDF_path in tqdm(paths):
+        "extracts text"
         PDF_path = str(PDF_path)
 
-        # convert pdf to png
-        PNG_path = ankiMediaFolder + PDF_path[len(PDF_dir):] + ".png"
-        subprocess.run(["convert", "-density", "300","-quiet", PDF_path, PNG_path],stdout=subprocess.PIPE)
-
         # extract text of pdf
-        PDF_text = subprocess.run(["pdftotext", "-layout","-nopgbrk","-enc","UTF-8",PDF_path, "-"],stdout=subprocess.PIPE, encoding='utf-8').stdout
-
-        PDF_text = str(PDF_text)
+        PDF_text = subprocess.run(["pdftotext", "-layout","-nopgbrk","-enc","UTF-8",PDF_path, "-"],stdout=subprocess.PIPE, encoding='utf-8')
+        PDF_text = str(PDF_text.stdout)
         PDF_text = PDF_text.replace("\n","<br>") # tries to keep formatting in anki
-        PDF_text = PDF_text.replace(" ","&nbsp;")
+        #PDF_text = PDF_text.replace(" ","&nbsp;")
 
-        # sends card into anki
-        PNG_name = str(''.join([PDF_path[len(PDF_dir):],".png"]))
-        sendPDFPageToAnki(PNG_name, PDF_text)
+        JPG_name = f"{PDF_path[len(PDF_dir):]}.jpg"
+        if store_picture == "yes" :
+            sendToAnki(JPG_name, PDF_text)
+        else :
+            sendToAnki(f"Picture not embedded : \n{JPG_name}", PDF_text)
+
+
+
+print(f"Done! Duration = {time.process_time()}")
